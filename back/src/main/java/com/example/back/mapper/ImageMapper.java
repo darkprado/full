32327d3154f893be1e66e0cedf87bd32a8f1bd96
Melekhin <s.melekhin@bftcom.com>
2023-05-25 11:17:50 +0300

    fix image mapper

--- back/src/main/java/com/example/back/mapper/ImageMapper.java
+++ back/src/main/java/com/example/back/mapper/ImageMapper.java
@@ -4,9 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.zip.Deflater;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.mapstruct.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,31 +23,25 @@ public interface ImageMapper {
     Logger LOG = LoggerFactory.getLogger(ImageMapper.class);
 
     @Mapping(target = "postId", ignore = true)
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "imageBytes", ignore = true)
     @Mapping(target = "userId", source = "user.id")
-    @Mapping(target = "name", source = "file", qualifiedByName = "mapBlob")
-    @Mapping(target = "imageBytes", source = "file", qualifiedByName = "mapFileName")
     ImageModel toUserImage(MultipartFile file, User user);
 
     @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "imageBytes", ignore = true)
     @Mapping(target = "postId", source = "post.id")
-    @Mapping(target = "name", source = "file", qualifiedByName = "mapBlob")
-    @Mapping(target = "imageBytes", source = "file", qualifiedByName = "mapFileName")
     ImageModel toPostImage(MultipartFile file, Post post);
 
-
-    @Named("mapBlob")
-    default byte[] mapBlob(MultipartFile file) {
+    @AfterMapping
+    default void afterMapping(@MappingTarget ImageModel imageModel, MultipartFile file) {
         try {
-            return compressBytes(file.getBytes());
+            imageModel.setImageBytes(compressBytes(file.getBytes()));
         } catch (IOException ex) {
             LOG.error("Cannot get bytes in file");
         }
-        return null;
-    }
-
-    @Named("mapFileName")
-    default String mapFileName(MultipartFile file) {
-        return file.getOriginalFilename();
+        imageModel.setName(file.getOriginalFilename());
     }
 
     private byte[] compressBytes(byte[] data) {
